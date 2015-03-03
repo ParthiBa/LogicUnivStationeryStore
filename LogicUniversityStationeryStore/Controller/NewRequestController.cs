e32db79765ea0fc18ee5a3d082400ed4e586424a
ParthiBa <uparthiban@outlook.com>
2015-03-03 21:04:26 +0800

    dsdd

--- LogicUniversityStationeryStore/Controller/NewRequestController.cs
+++ LogicUniversityStationeryStore/Controller/NewRequestController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
 
 using LogicUniversityStationeryStore.DAO;
 
@@ -9,6 +10,11 @@ namespace LogicUniversityStationeryStore.Controller
 {
     public class NewRequestController
     {
+
+        string EmpId;
+        DateTime dateToday;
+        int requestID;
+        Request r;
         public string getAvailabeQuantity(string id)
         {
 
@@ -17,6 +23,60 @@ namespace LogicUniversityStationeryStore.Controller
                           select x.availableQty ).Single();
             return getMax.ToString();
         }
+        public string getAvailabeQuantitybyStationary(string stationay)
+        {
 
+            var getMax = (from inven in EntityBroker.getMyEntities().Inventories
+                          join stat in  EntityBroker.getMyEntities().Stationeries on inven.stationeryCode equals stat.code
+                          where stat.description==stationay
+                          select inven.availableQty).Single();
+            return getMax.ToString();
+        }
+
+
+        internal void createRequest(string empId)
+        {
+           r = new Request();
+            r.Employee = EntityBroker.getMyEntities().Employees.Find(empId);
+            r.dateOfApp = DateTime.Now;
+            r.Department = r.Employee.Department;
+            r.lastUpdate = DateTime.Now;
+            r.status = "Pending";
+            EntityBroker.getMyEntities().Requests.Add(r);
+            EntityBroker.getMyEntities().SaveChanges();
+            requestID = r.id;
+
+        
+
+       
+
+        }
+
+        internal void createRequestDetail()
+        {
+           
+        }
+
+        internal void createRequestDetail(DataTable dt)
+        {
+             foreach(DataRow dr in dt.Rows)
+             {
+                 RequestDetail rd = new RequestDetail();
+                 rd.Request = r;
+               rd.neededQuantity=Convert.ToInt32(  dr["Quantity"].ToString()) ;
+               rd.Stationery = getStat(dr["StationaryId"].ToString());
+               EntityBroker.getMyEntities().RequestDetails.Add(rd);
+             }
+
+             EntityBroker.getMyEntities().SaveChangesAsync();
+        }
+
+        private Stationery getStat (string id)
+        {
+            var q = from x in EntityBroker.getMyEntities().Stationeries
+                    where x.code.Equals(id)
+                    select x ;
+            return q.FirstOrDefault < Stationery > ();
+        }
     }
 }
